ec768bfed7320ab53cf8ff3bde00fe96f1addc30
GeekXtop <103322869+GeekXtop@users.noreply.github.com>
2022-11-04 06:12:49 +0800

    update

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,42 +1,74 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import personsService from './services/persons';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
-  const [newNumber, setnewNumber] = useState('');
+  const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [message, setMessage] = useState(null);
 
-  const hook = () => {
-    axios.get('http://localhost:3001/persons').then((response) => {
-      console.log('promise fulfilled');
-      setPersons(response.data);
-    });
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then((initialPersons) => setPersons(initialPersons));
+  }, []);
+
+  const notify = (msg, type = 'info') => {
+    setMessage({ msg, type });
+    setTimeout(() => {
+      setMessage(null);
+    }, 3000);
   };
-  useEffect(hook, []);
 
   const addPerson = (event) => {
     event.preventDefault();
-    console.log('button clicked', event.target);
-
-    let namesSet = new Set();
-    persons.map((person) => namesSet.add(person.name));
-
-    if (namesSet.has(newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
-
     const nameObject = {
       name: newName,
       number: newNumber,
     };
-    console.log('', nameObject);
-    setPersons(persons.concat(nameObject));
+
     setNewName('');
+    setNewNumber('');
+
+    const existingPerson = persons.find((p) => p.name === nameObject.name);
+
+    if (existingPerson) {
+      const ok = window.confirm(
+        `${existingPerson.name} is already added to phonebook, update the number?`
+      );
+      if (ok) {
+        personsService
+          .update(existingPerson.id, {
+            ...existingPerson,
+            number: newNumber,
+          })
+          .then((savedPerson) => {
+            setPersons(
+              persons.map((p) => (p.id === existingPerson.id ? savedPerson : p))
+            );
+            notify(`Updated info of ${savedPerson.name}`);
+          })
+          .catch((error) => {
+            notify(
+              `the person '${existingPerson.name}' was had already been from the server`,
+              'alert'
+            );
+            setPersons(persons.filter((p) => p.id !== existingPerson.id));
+          });
+
+        return;
+      }
+    }
+
+    personsService.create(nameObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      notify(`Added ${returnedPerson.name}`);
+    });
   };
 
   const personsToShow =
@@ -52,17 +84,27 @@ const App = () => {
   };
   const handleNumberChange = (event) => {
     console.log(event.target.value);
-    setnewNumber(event.target.value);
+    setNewNumber(event.target.value);
+  };
+  const deletePerson = (id) => {
+    const toDelete = persons.find((p) => p.id === id);
+    const ok = window.confirm(`Delete ${toDelete.name}`);
+    if (ok) {
+      personsService.remove(id).then(() => {
+        setPersons(persons.filter((p) => p.id !== id));
+        notify(`Deleted ${toDelete.name}`);
+      });
+    }
   };
 
   return (
     <div>
+      <Notification message={message} />
       <h2>Phonebook</h2>
       <Filter
         value={filter}
         handleChange={({ target }) => setFilter(target.value)}
       />
-
       <h2>add a new</h2>
       <PersonForm
         addPerson={addPerson}
@@ -72,7 +114,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
     </div>
   );
 };
